4a381ac8faa66a63e5833e97d0f70cec6683bd6b
the-lotuseater <agbirhade1@gmail.com>
2021-05-24 01:04:33 -0500

    Fix Failing tests

--- src/App.test.js
+++ src/App.test.js
@@ -4,12 +4,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/React is so cool. Check it out here. More dummy text./i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/React is so cool. Check it out here. More dummy text./i);
   expect(linkElement).toBeInTheDocument();
 });
